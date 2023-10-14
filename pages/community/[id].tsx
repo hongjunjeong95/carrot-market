116d6647f4be7edd2b2f67bc1fd5508c7c59e33d
hongjunjeong <hongjunjeong95@gmail.com>
2023-10-14 21:12:28 +0900

    Create stream

--- pages/community/[id].tsx
+++ pages/community/[id].tsx
@@ -93,10 +93,11 @@ const CommunityPostDetail: NextPage = () => {
             <p className="text-sm font-medium text-gray-700">
               {data?.post?.user?.name}
             </p>
-            <Link href={`/users/profiles/${data?.post?.user?.id}`}>
-              <a className="text-xs font-medium text-gray-500">
-                View profile &rarr;
-              </a>
+            <Link
+              href={`/users/profiles/${data?.post?.user?.id}`}
+              className="text-xs font-medium text-gray-500"
+            >
+              View profile &rarr;
             </Link>
           </div>
         </div>
