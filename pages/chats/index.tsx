81127fbd934baf971650063d7dd6926c04a00d36
hongjunjeong <hongjunjeong95@gmail.com>
2023-10-15 04:16:28 +0900

    Static generation

--- pages/chats/index.tsx
+++ pages/chats/index.tsx
@@ -6,7 +6,7 @@ const Chats: NextPage = () => {
   return (
     <Layout hasTabBar title="채팅">
       <div className="divide-y-[1px] ">
-        {[1, 1, 1, 1, 1, 1, 1].map((_, i) => (
+        {[1].map((_, i) => (
           <Link
             href={`/chats/${i}`}
             key={i}
