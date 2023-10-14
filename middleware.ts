012a8b826c04cbc8111b3f6ecc2f196da2542148
hongjunjeong <hongjunjeong95@gmail.com>
2023-10-15 01:35:35 +0900

    Create middleware

---
+++ middleware.ts
@@ -0,0 +1,18 @@
+import type { NextRequest, NextFetchEvent } from "next/server";
+import { NextResponse, userAgent } from "next/server";
+
+export function middleware(req: NextRequest, ev: NextFetchEvent) {
+  const { isBot } = userAgent(req);
+  if (isBot) {
+    return new Response("Plz don't be a bot. Be human.", { status: 403 });
+  }
+  if (!req.cookies.has("carrotsession") && !req.url.includes("/enter")) {
+    req.nextUrl.searchParams.set("from", req.nextUrl.pathname);
+    req.nextUrl.pathname = "/enter";
+    return NextResponse.redirect(req.nextUrl);
+  }
+}
+
+export const config = {
+  matcher: ["/((?!api|_next/static|favicon.ico).*)"],
+};
