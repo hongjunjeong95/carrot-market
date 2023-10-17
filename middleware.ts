3908d5883127b250c5b883866249729b75846fc3
hongjunjeong <hongjunjeong95@gmail.com>
2023-10-17 19:20:08 +0900

    Bug fix

--- middleware.ts
+++ middleware.ts
@@ -6,11 +6,11 @@ export function middleware(req: NextRequest, ev: NextFetchEvent) {
   if (isBot) {
     return new Response("Plz don't be a bot. Be human.", { status: 403 });
   }
-  // if (!req.cookies.has("carrotsession") && !req.url.includes("/enter")) {
-  //   req.nextUrl.searchParams.set("from", req.nextUrl.pathname);
-  //   req.nextUrl.pathname = "/enter";
-  //   return NextResponse.redirect(req.nextUrl);
-  // }
+  if (!req.cookies.has("carrotsession") && !req.url.includes("/enter")) {
+    req.nextUrl.searchParams.set("from", req.nextUrl.pathname);
+    req.nextUrl.pathname = "/enter";
+    return NextResponse.redirect(req.nextUrl);
+  }
 }
 
 export const config = {
